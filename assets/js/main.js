e1906e254d1abbe24cca35d73e9c71b806218672
YunisGarayev <79d2mw0@code.edu.az>
2023-04-26 20:02:57 -0700

    27-04 task

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+
+// birinci usul
+// var string = 'Yunis Garayev is considered a student of Code academy',
+//     searchFor = prompt(),
+//     count = 0,
+//     pos = string.indexOf(searchFor);
+
+// while (pos > -1) {
+//     count++;
+//     pos = string.indexOf(searchFor, ++pos);
+// }
+
+// console.log(count);
+
+
+
+
+// ikici usul
+
+// let string = "Yunis Garayev is considered a student of Code academy";
+// let searchFor =prompt();
+
+// let count = string.split(searchFor).length - 1;
+// console.log(count); 
